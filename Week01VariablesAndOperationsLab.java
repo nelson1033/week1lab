29ac990989d8c8a09da028d60c458731ceb907a9
nelson1033 <nelson1033@outlook.com>
2024-01-26 12:15:24 -0700

    firstcommit

---
+++ Week01VariablesAndOperationsLab.java
@@ -0,0 +1,62 @@
+//
+// Copyright (c) 2023 Promineo Tech
+// Author:  Promineo Tech Academic Team
+// Subject:  Variables & Operations Lab
+// Java Week 01 Lab  
+
+package week01;
+
+public class Week01VariablesAndOperationsLab{
+
+	public static void main(String[] args) {
+
+		// 1. Create a variable to hold the quantity of 
+		//		available plane seats left on a flight		
+		int availablePlaneSeats = 50;
+		
+		// 2. Create a variable to hold the cost of groceries at checkout
+		double costOfGroceriesAtCheckout = 105.50;
+		
+		// 3. Create a variable to hold a person's middle initial
+		char personsMiddleInitial = 'P';
+	
+		// 4. Create a variable to hold true if it's hot outside 
+		//		and false if it's cold outside
+		boolean isItHotOutside = true;
+		
+		// 5. Create a variable to hold a customer's first name
+		String customersFirstName = "Paul";
+		
+		// 6. Create a variable to hold a street address
+		String streetAddress = "456 W Hwy 1";
+
+		// 7. Print all variables to the console
+		System.out.println(customersFirstName);
+
+		// 8. A customer booked 2 plane seats, 
+		//		remove 2 seats from the available seats variable
+	
+
+		// 9. Impulse candy bar purchase, add 2.15 to the grocery total
+		// 		costOfGroceries = costOfGroceries + 2.15;
+
+
+		// 10.  The birth certificate was printed incorrectly, 
+		//		change the middle initial to something else
+		
+
+		// 11.  The season has changed, update the hot outside 
+		//			variable to be opposite of what it was
+
+
+		// 12.  Create a new variable called full name using the customer's first name, 
+		//			the middle initial, and a last name of your choice
+	
+
+		// 13.  Print a line to the console that introduces the customer and says they live
+		// 			at the address variable
+
+		
+		
+	}
+}
